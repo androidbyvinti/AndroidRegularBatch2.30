a8af4bf9fd247c4313dcb82a188a2c296c793ca6
androidbyvinti <32050504+androidbyvinti@users.noreply.github.com>
2018-05-23 13:46:32 +0530

    Add files via upload

---
+++ 23 May- Class Data/SqliteDatabase/sqlitedatabase/Details.kt
@@ -0,0 +1,18 @@
+package com.bmpl.sqlitedatabase_nishamohit
+
+//POJO --> pure old java object --> Java Bean
+
+class Details {
+    var id: Int = 0
+    var name: String? = null
+    var phn: Long = 0
+
+    //Details(){}
+    constructor() {}
+
+    constructor(id: Int, name: String, phn: Long) {
+        this.id = id
+        this.name = name
+        this.phn = phn
+    }
+}
